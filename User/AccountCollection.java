56eb07f363b93881bef716673a70fa7dcd225f43
Fatima Hejazi <>
2021-05-02 10:19:00 +0000

    Added AccountCollection and ProfileCollection classes

---
+++ User/AccountCollection.java
@@ -0,0 +1,46 @@
+package User;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class AccountCollection(){
+
+    List<Account> accounts;
+
+    public AccountCollection(){
+
+    }
+    public boolean addAccount(Account acc) {
+        // comparison then add
+
+            return false;
+        }
+
+    public boolean removeAccount(Account acc) {
+        return false;
+    }
+
+    public void modifyAccount(Account search, Account newAcc) {
+    }
+
+    public Account getAccountWithUsername(String username) {
+
+        return null;
+    }
+
+
+    public void sortAccounts(Comparator comp) {
+    }
+
+    public ListIterator getAccounts() {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+
+}
---
+++ User/ProfileCollection.java
@@ -0,0 +1,46 @@
+package User;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class ProfileCollection(){
+
+    List<Profile> profiles;
+
+    public ProfileCollection(){
+
+    }
+    public boolean addProfile(Profile p) {
+        // comparison then add
+
+            return false;
+        }
+
+    public boolean removeProfile(Profile p) {
+        return false;
+    }
+
+    public void modifyProfile(Profile search, Profile newProfile) {
+    }
+
+    public Profile getProfileWithEmail(String email) {
+
+        return null;
+    }
+
+
+    public void sortProfiles(Comparator comp) {
+    }
+
+    public ListIterator getProfiles() {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+
+}
